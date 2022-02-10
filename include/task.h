0ff7673de50b05eb189052e3cc4b25f030a9b37b
hanSomBorISkogen <94908683+hanSomBorISkogen@users.noreply.github.com>
2022-02-10 08:15:59 +0100

    Preparing for lab3

---
+++ include/task.h
@@ -0,0 +1,6 @@
+// The interface for the .
+#ifndef TASK_H_ /* Include guard */
+#define TASK_H_
+
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#define SECPERTASK 2 // On average there will be one print task in 2 seconds.
+
+static void printWelcomeMessage(); // This function prints a short welcome message.
+
+void main() { 
+    printWelcomeMessage(); // Prints welcome message to the screen.
+    
+    fflush(stdin);
+    getchar();
+}
+static void printWelcomeMessage() { // Prints welcome message to the screen.
+    puts("****************************************************************************************************\n"
+        "Welcome!\n"
+        "This is a program that uses a queue implemented as a linkedlist to create a simulation of a printer.\n"
+        "Please enjoy!\n"
+        "****************************************************************************************************");
+}
